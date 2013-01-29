da8dd5cd95838963ed8c512edef0db60c38147ee
wim luts <wim.luts@hotmail.com>
2013-01-29 15:54:27 +0100

    update met sqliteDB

---
+++ MapkitAppCitygame/checkConnection.h
@@ -0,0 +1,13 @@
+//
+//  checkConnection.h
+//  MapkitAppCitygame
+//
+//  Created by Wim on 28/01/13.
+//  Copyright (c) 2013 Wim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface checkConnection : NSObject
+
+@end
---
+++ MapkitAppCitygame/checkConnection.m
@@ -0,0 +1,13 @@
+//
+//  checkConnection.m
+//  MapkitAppCitygame
+//
+//  Created by Wim on 28/01/13.
+//  Copyright (c) 2013 Wim. All rights reserved.
+//
+
+#import "checkConnection.h"
+
+@implementation checkConnection
+
+@end
